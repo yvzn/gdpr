243a9c73ccbd5df92aa5de83c098301fa7b5df5e
Yvan RAZAFINDRAMANANA <yvan.razafindramanana@younited-credit.fr>
2026-03-20 18:18:38 +0100

    feat(app): init app shell

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('app');
+export class App implements AfterViewInit {
+  ngAfterViewInit(): void {
+    initFlowbite();
+  }
 }
